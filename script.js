b3fd4a4b1bb489bbb72270a23dbe9584c228db03
Amber Orrange <orrangeamber@gmail.com>
2022-02-04 18:22:28 -0600

    ops

---
+++ script.js
@@ -0,0 +1,71 @@
+
+let firstName = 'Amber';
+
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun)
+
+// console.log(typeof true);
+// console.log(typeof 23)
+// console.log(typeof 'Jonas')
+// console.log(typeof javascriptIsFun)
+
+javascriptIsFun = 'this is the story of my life';
+console.log(typeof javascriptIsFun);
+
+// undefined variable
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(year);
+// console.log(typeof year);
+
+
+// console.log(typeof null);
+
+const test = 'this is a test';
+
+
+
+
+// lecture assignments
+const country = 'USA';
+const continent = 'North America';
+let population = 2500000;
+const isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof language);
+console.log(typeof population);
+console.log(typeof country);
+language = 'English';
+console.log(typeof language);
+
+let halfPopulation = population / 2;
+console.log(halfPopulation)
+
+// population++;
+// console.log(population);
+// console.log(population > 6000000)
+// let avg = 33000000;
+// console.log(population < avg);
+
+// console.log(country + ' is in ' + continent + ', and its ' + population + ' people speak ' + language + '.')
+
+const now = 2037
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+// console.log(ageJonas, ageSarah);
+
+// console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3
+
+const lastName = 'Orrange';
+// console.log(firstName + ' ' + lastName);
+
+
