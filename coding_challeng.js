3b1d61bb99d5a146e6f29f836dcbe4b8e4ef2c24
Amber Orrange <orrangeamber@gmail.com>
2022-02-04 18:49:06 -0600

    challenge

---
+++ coding_challeng.js
@@ -0,0 +1,24 @@
+// let markMass = 78;
+// let markHeight = 1.69;
+
+// let johnMass = 92;
+// let johnHeight = 1.88;
+
+
+markMass = 95;
+markHeight = 1.88;
+
+johnMass = 85;
+johnHeight = 1.76;
+
+let markBMI = markMass / (markHeight ** 2);
+let johnBMI = johnMass / (johnHeight ** 2);
+
+console.log(markBMI);
+console.log(johnBMI);
+
+let markHigherBMI = markBMI > johnBMI;
+
+console.log(markHigherBMI);
+
+
